3d692237048dedfb67baa86fda56b15ec5447cd2
Admin <admin@Admins-MacBook-Pro.local>
2023-09-10 15:38:19 +0600

    first commit

---
+++ bin/lesson_3_3.dart
@@ -0,0 +1,27 @@
+import 'package:lesson_3_3/lesson_3_3.dart' as lesson_3_3;
+import 'dart:io';
+
+void main() {
+  print("Введите число: ");
+  var a = stdin.readLineSync()!;
+  var exe = int.parse(a) * 1;
+  var exe1 = int.parse(a) * 2;
+  var exe2 = int.parse(a) * 3;
+  var exe3 = int.parse(a) * 4;
+  var exe4 = int.parse(a) * 5;
+  var exe5 = int.parse(a) * 6;
+  var exe6 = int.parse(a) * 7;
+  var exe7 = int.parse(a) * 8;
+  var exe8 = int.parse(a) * 9;
+  var exe9 = int.parse(a) * 10;
+  print(exe);
+  print(exe1);
+  print(exe2);
+  print(exe3);
+  print(exe4);
+  print(exe5);
+  print(exe6);
+  print(exe7);
+  print(exe8);
+  print(exe9);
+}
